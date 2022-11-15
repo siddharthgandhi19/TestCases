05f035c8b6be80b1fcbdd1c424c95919b06cefd2
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-15 17:26:55 +0530

    [Siddharth]Add: One generic method and  find the maximum

--- FindMaxNumTest/FindMaxTest.cs
+++ FindMaxNumTest/FindMaxTest.cs
@@ -7,60 +7,60 @@ namespace FindMaxNumTest
     public class Tests
     {
         FindMaxNum maxNum = new FindMaxNum();
-
+        FindMaxGeneric generic = new FindMaxGeneric();
         [Test]
         public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFistMax()
         {
-            int result = maxNum.FindMaxInteger(15, 5, 10);
+            int result = generic.FindMax(15, 5, 10);
             Assert.AreEqual(15, result);
         }
         [Test]
         public void GivenMaxSecondNum_WhenAnalysed_ShouldReturnSecondMax()
         {
-            int result = maxNum.FindMaxInteger(5, 15, 10);
+            int result = generic.FindMax(5, 15, 10);
             Assert.AreEqual(15, result);
         }
         [Test]
         public void GivenMaxThirdNum_WhenAnalysed_ShouldReturnThirdMax()
         {
-            int result = maxNum.FindMaxInteger(5, 10, 15);
+            int result = generic.FindMax(5, 10, 15);
             Assert.AreEqual(15, result);
         }
 
         [Test]
         public void GivenMaxFirstNumFloat_WhenAnalysed_ShouldReturnFistMax()
         {
-            float result = maxNum.FindMaxFloat(15.02f, 5.01f, 10.1f);
+            float result = generic.FindMax(15.02f, 5.01f, 10.1f);
             Assert.AreEqual(15.02f, result);
         }
         [Test]
         public void GivenMaxSecondNumFloat_WhenAnalysed_ShouldReturnSecondMax()
         {
-            float result = maxNum.FindMaxFloat(5.01f, 15.02f, 10.1f);
+            float result = generic.FindMax(5.01f, 15.02f, 10.1f);
             Assert.AreEqual(15.02f, result);
         }
         [Test]
         public void GivenMaxThirdNumFloat_WhenAnalysed_ShouldReturnThirdMax()
         {
-            float result = maxNum.FindMaxFloat(5.01f, 10.1f, 15.02f);
+            float result = generic.FindMax(5.01f, 10.1f, 15.02f);
             Assert.AreEqual(15.02f, result);
         }
         [Test]
         public void GivenMaxFirstNumString_WhenAnalysed_ShouldReturnFistMax()
         {
-            string result = maxNum.FindMaxString("Peach","Apple", "Banana");
+            string result = generic.FindMax("Peach","Apple", "Banana");
             Assert.AreEqual("Peach", result);
         }
         [Test]
         public void GivenMaxSecondNumString_WhenAnalysed_ShouldReturnSecondMax()
         {
-            string result = maxNum.FindMaxString( "Apple", "Peach", "Banana");
+            string result = generic.FindMax( "Apple", "Peach", "Banana");
             Assert.AreEqual("Peach", result);
         }
         [Test]
         public void GivenMaxThirdNumString_WhenAnalysed_ShouldReturnThirdMax()
         {
-            string result = maxNum.FindMaxString("Apple", "Banana" , "Peach");
+            string result = generic.FindMax("Apple", "Banana" , "Peach");
             Assert.AreEqual("Peach", result);
         }
     }
---
+++ FindMaximumNumber/FindMaxGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FindMaximumNumber
+{
+    public class FindMaxGeneric
+    {
+        public T FindMax<T>(T a, T b, T c) where T : IComparable
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+                return a;
+            if (b.CompareTo(c) > 0 && b.CompareTo(a) > 0)
+                return b;
+            else
+                return c;
+        }
+    }
+}
