70fac21989c744412340a10ce488f823b27ebe85
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-15 17:00:15 +0530

    [Siddharth]Add: Given three Strings find the maximum

--- FindMaxNumTest/FindMaxTest.cs
+++ FindMaxNumTest/FindMaxTest.cs
@@ -1,12 +1,13 @@
 using FindMaximumNumber;
 using NUnit.Framework;
+using System;
 
 namespace FindMaxNumTest
 {
     public class Tests
     {
         FindMaxNum maxNum = new FindMaxNum();
-        
+
         [Test]
         public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFistMax()
         {
@@ -43,6 +44,24 @@ namespace FindMaxNumTest
         {
             float result = maxNum.FindMaxFloat(5.01f, 10.1f, 15.02f);
             Assert.AreEqual(15.02f, result);
+        }
+        [Test]
+        public void GivenMaxFirstNumString_WhenAnalysed_ShouldReturnFistMax()
+        {
+            string result = maxNum.FindMaxString("Peach","Apple", "Banana");
+            Assert.AreEqual("Peach", result);
+        }
+        [Test]
+        public void GivenMaxSecondNumString_WhenAnalysed_ShouldReturnSecondMax()
+        {
+            string result = maxNum.FindMaxString( "Apple", "Peach", "Banana");
+            Assert.AreEqual("Peach", result);
+        }
+        [Test]
+        public void GivenMaxThirdNumString_WhenAnalysed_ShouldReturnThirdMax()
+        {
+            string result = maxNum.FindMaxString("Apple", "Banana" , "Peach");
+            Assert.AreEqual("Peach", result);
         }
     }
 }
--- FindMaximumNumber/FindMaxNum.cs
+++ FindMaximumNumber/FindMaxNum.cs
@@ -24,5 +24,15 @@
                 return c;
 
         }
+        public string FindMaxString(string a, string b, string c)
+        {
+
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+                return a;
+            if (b.CompareTo(c) > 0 && b.CompareTo(a) > 0)
+                return b;
+            else
+                return c;
+        }
     }
 }
